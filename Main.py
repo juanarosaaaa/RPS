0d180aedfb7e0291e35a3b2ceb91cbdfca937741
johndominic <johndominicjasmin@gmail.com>
2021-11-08 19:28:01 +0800

    Add images need for Main menu and choose hand window

--- Main.py
+++ Main.py
@@ -1,38 +1,12 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-import sys
 
+from PyQt5 import QtCore, QtGui, QtWidgets
+from MainMenu import Ui_MainWindow
 
-class MainWindow(QMainWindow):
-
-
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.setGeometry(200, 200, 300, 300)
-        self.setWindowTitle("Rock Paper Scissors")
-        self.initUI()
-
-
-    def initUI(self):
-       self.label = QtWidgets.QLabel(self)
-       self.label.setText("Text Label")
-       self.label.move(50, 50)
-
-
-       self.button = QtWidgets.QPushButton(self)
-       self.button.setText("Push Button")
-       self.button.clicked.connect(self.onClick)
-
-
-    def onClick(self):
-        self.label.setText("Changed!")
-
-
-def window():
-    app = QApplication(sys.argv)
-    win = MainWindow()
-    win.showMaximized()
-    sys.exit(app.exec())
-
-    
-window()
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.showMaximized()
+    sys.exit(app.exec_())
