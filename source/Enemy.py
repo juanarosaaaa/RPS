1d28b4201dec75aa3c228bf028481354197dafe3
John_Dominic <johndominicjasmin@gmail.com>
2021-11-13 23:44:48 +0800

    Add enemy, add generated class file

--- source/Enemy.py
+++ source/Enemy.py
@@ -3,15 +3,15 @@ import Rock
 import Scissor
 import Paper
 from random import randint
-from HandStrategy import Hand_Strategy
+from GameStrategy import Game_Strategy
 
 class Enemy_Hand():
-    def create_enemy() -> Hand_Strategy:
+    def create_enemy() -> Game_Strategy:
         n = randint(0,2)
         hand_result ={
-            0:Rock.Hand_Rock,
-            1:Scissor.Hand_Scissor,
-            2:Paper.Hand_Paper
+            0:Rock.Hand_Rock(),
+            1:Scissor.Hand_Scissor(),
+            2:Paper.Hand_Paper()
         }
     
         return hand_result.get(n)
