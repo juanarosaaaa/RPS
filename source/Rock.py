32b9c6d1972e67a54e797606c614f83c7e95147c
John_Dominic <johndominicjasmin@gmail.com>
2021-11-13 16:52:44 +0800

    Change refactoring, add more fucntions in GameStrategy inteface

--- source/Rock.py
+++ source/Rock.py
@@ -1,14 +1,31 @@
-from HandStrategy import Hand_Strategy as Parent
-import Paper
+import GameStrategy as Parent
+import Paper 
+import Rock
+import RockWinDialog
+import RockLoseDialog
+import RockDrawDialog
 import Scissor
-
+from PyQt5 import QtCore, QtGui, QtWidgets
+from images import ResourceImage
 
 class Hand_Rock(Parent):
-    def play_against(handStrategy):
-       
-        hand_result = {
+
+    def play_against(self,handStrategy) :
+        self.strategy = handStrategy
+
+
+    def get_score(self):
+        self.hand_result = {
             Scissor.Hand_Scissor:1,
             Paper.Hand_Paper:-1,
             __class__:0}
+        return self.hand_result.get(self.strategy)    
 
-        return hand_result.get(handStrategy)    
+    def set_ui_result(self):
+        self.window = QtWidgets.QMainWindow()
+        self.hand_window = {
+            Scissor.Hand_Scissor:RockWinDialog.Ui_DialogRockWin,
+            Paper.Hand_Paper: RockLoseDialog.Ui_DialogRockLose,
+            __class__:RockDrawDialog.Ui_Dialog}
+        self.ui = self.hand_window.get(self.strategy)
+        self.ui.setupUi(self.window)
